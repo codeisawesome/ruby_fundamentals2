1a2f8e9ef9fa517cd4f879bf4d2b59136c8e519f
Rui Dong <rui.dong.jh@gmail.com>
2016-08-09 14:10:00 -0400

    totaled the number of students in all corhorts except cohort 2

--- exercise7.rb
+++ exercise7.rb
@@ -25,3 +25,10 @@ puts ''
 #deleting corhort 2
 students.delete_if{|k,v| k == :cohort2 }
 students.each{|x,y| puts "#{x}: #{y}"}
+
+#summing all values of hash
+array = students.values
+puts ''
+
+puts "The total number of people in class"
+puts array.inject(:+)
