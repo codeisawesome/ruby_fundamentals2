c03ae6a3450b85a2b25e363fa492e466e34a9ecc
Rui Dong <rui.dong.jh@gmail.com>
2016-08-09 12:10:10 -0400

    incrased each cohort size by 5%

--- exercise7.rb
+++ exercise7.rb
@@ -4,10 +4,19 @@ students = {
   :cohort3 => 22
 }
 
+#printing out key/value pair
 students.each{|x,y| puts "#{x}: #{y}"}
-
 puts ''
 
+#Adding new cohort4 with 43 students
 students.merge!(cohort4: 43)
-
 students.each{|x,y| puts "#{x}: #{y}"}
+puts''
+
+#printing out the names of each cohort
+puts students.keys
+puts ''
+
+#increasing class size by 5%
+newstudents = Hash[students.map {|k,v| [k, v * 1.05]}]
+puts newstudents.each{|x,y| puts "#{x}: #{y}"}
